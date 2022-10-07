b49797c1302b0854a5c8d6e8779d8541f5040d0e
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-10-07 13:57:25 +0400

    Add files via upload

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -11,47 +11,45 @@ class BST:
             self.value = value
             return
 
-        if self.value == value:
-            return 
-
-        if self.value > value:
+        if self.value >= value:
             if self.left:
                 self.left.insert(value)
-                return
 
-            self.left = BST(value)
-            return
+            else:
+                self.left = BST(value)
+
+            return    
 
         if self.value < value:
             if self.right:
                 self.right.insert(value)
-                return
 
-            self.right = BST(value)
+            else:
+                self.right = BST(value)
             return
    
 
     def postorder(self, values=[]):
 
-        if self.left != None:
+        if self.left:
             self.left.postorder(values)
 
-        if self.right != None:
+        if self.right:
             self.right.postorder(values)
 
-        if self.value != None:
+        if self.value:
             values.append(self.value)
 
         return values
 
     def preorder(self, values=[]):
-        if self.value != None:
+        if self.value:
             values.append(self.value)
 
-        if self.left != None:
+        if self.left:
             self.left.preorder(values)
 
-        if self.right != None:
+        if self.right:
             self.right.preorder(values)
 
         return values                                     
@@ -68,10 +66,12 @@ class BST:
 
 bst = BST()
 
-l1 = [6, 5, 7, 12, 8, 4, 3, 2, 13]
-
-for num in l1:
-    bst.insert(num)
+bst.insert(7)
+bst.insert(7)
+bst.insert(8)
+bst.insert(2)
+bst.insert(5)
+bst.insert(2)
            
 print(bst.get_number_of_nodes())
 print(bst.get_root_data())
