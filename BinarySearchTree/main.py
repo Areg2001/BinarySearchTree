643af895d8387ff2b028f7b9a7e933bff049ef0e
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-10-07 18:47:20 +0400

    Add files via upload

--- BinarySearchTree/main.py
+++ BinarySearchTree/main.py
@@ -6,6 +6,9 @@ class BST:
         self.value = value
         BST.counter += 1
 
+    def __del__(self):
+        BST.counter -= 1
+
     def insert(self, value):
         if not self.value:
             self.value = value
@@ -16,9 +19,7 @@ class BST:
                 self.left.insert(value)
 
             else:
-                self.left = BST(value)
-
-            return    
+                self.left = BST(value) 
 
         if self.value < value:
             if self.right:
@@ -26,11 +27,41 @@ class BST:
 
             else:
                 self.right = BST(value)
-            return
-   
 
+    def clear(self):
+       self.right = None
+       self.left = None
+       self.value = None        
+          
+
+    def contains(self, value):
+        if self.value == value:
+            return True
+
+        if value <= self.value:
+            if not self.left:
+                return False
+
+            return self.left.contains(value)
+
+        else:
+            if not self.right:
+                return False
+
+            return self.right.contains(value)
+           
+    def inorder(self, values=[]):
+        if self.left:
+            self.left.inorder(values)
+
+        if self.value:
+            values.append(self.value)
+
+        if self.right:
+            self.right.inorder(values)
+        return values            
+             
     def postorder(self, values=[]):
-
         if self.left:
             self.left.postorder(values)
 
@@ -52,31 +83,33 @@ class BST:
         if self.right:
             self.right.preorder(values)
 
-        return values                                     
-
+        return values
+  
     def get_number_of_nodes(self):
         return self.counter
 
     def get_root_data(self):
         return self.value    
 
-
     def __str__(self):
-        return f"{self.value}"   
+        return f"{self.value}" 
 
+             
 bst = BST()
-
 bst.insert(7)
-bst.insert(7)
+bst.insert(6)
 bst.insert(8)
 bst.insert(2)
 bst.insert(5)
 bst.insert(2)
-           
-print(bst.get_number_of_nodes())
-print(bst.get_root_data())
+print(bst.inorder())
+print(bst.contains(1)) 
 print(bst.postorder())
 print(bst.preorder())
+print(bst.get_root_data())
+print(bst.get_number_of_nodes())
+bst.clear()     
+
 
 
 
